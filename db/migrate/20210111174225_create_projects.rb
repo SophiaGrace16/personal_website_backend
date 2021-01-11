b7e4068b279cd73e232a95c4fdb2b36ea5d91e9e
SophiaGrace16 <sophia.g.white@outlook.com>
2021-01-11 12:56:25 -0500

    adding projects files

---
+++ db/migrate/20210111174225_create_projects.rb
@@ -0,0 +1,13 @@
+class CreateProjects < ActiveRecord::Migration[6.0]
+  def change
+    create_table :projects do |t|
+      t.string :projectName
+      t.string :imagelink
+      t.text :summary
+      t.string :githubLink
+      t.string :demoLink
+
+      t.timestamps
+    end
+  end
+end
